e542d58fd8f13de870ea38e07df870706bf3993b
Belykh Andrey (AB7) <belykhandrey@mail.ru>
2025-06-14 12:58:24 +0300

    Corrected 'default' vertex and fragment shaders.

--- dist/bin/shaders/default/frag.glsl
+++ dist/bin/shaders/default/frag.glsl
@@ -1,5 +1,15 @@
-layout (location = 0) out vec4 o_color;
+layout(location = 0) out vec4 OutColor;
 
-void main() {
-    o_color = vec4(1.0, 1.0, 0.0, 1.0);
+in vec3 DrawPos;
+in vec2 DrawTexCoord;
+in vec3 DrawNormal;
+in vec4 DrawColor;
+
+uniform vec3 Ka;
+uniform vec4 KdTrans;
+uniform vec4 KsPh;
+
+void main()
+{
+  OutColor = vec4(Ka * DrawColor.xyz, KdTrans.w); 
 }
--- dist/bin/shaders/default/vert.glsl
+++ dist/bin/shaders/default/vert.glsl
@@ -1,5 +1,23 @@
-layout (location = 0) in vec2 a_pos;
+layout(location = 0) in vec3 InPosition;
+layout(location = 1) in vec2 InTexCoord;
+layout(location = 2) in vec3 InNormal;
+layout(location = 3) in vec4 InColor;
 
-void main() {
-    gl_Position = vec4(a_pos, 0.0, 1.0);
+out vec3 DrawPos;
+out vec2 DrawTexCoord;
+out vec3 DrawNormal;
+out vec4 DrawColor;
+
+uniform mat4 MatrWVP;
+uniform mat4 MatrW;
+
+void main( void ) 
+{
+  gl_Position = MatrWVP * vec4(InPosition, 1);
+  mat4 MatrWInv = inverse(transpose(MatrW));
+
+  DrawPos = (MatrW * vec4(InPosition, 1.0)).xyz;
+  DrawTexCoord = InTexCoord;
+  DrawNormal = mat3(MatrWInv) * normalize(InNormal);
+  DrawColor = vec4(InColor);
 }
